e17f18b0e5d15bf040d3d6418a5a4625f70833e4
adeci <alex.decious@gmail.com>
2023-12-25 16:59:17 -0500

    x11link script feedback

--- .scripts/x11link.sh
+++ .scripts/x11link.sh
@@ -5,3 +5,13 @@ sudo rm -r /tmp/.X11-unix
 
 # Create a symbolic link to /mnt/wslg/.X11-unix in /tmp
 ln -s /mnt/wslg/.X11-unix /tmp/.X11-unix
+
+# Check and print the current X server running, should be X0 socket
+output=$(ls /tmp/.X11-unix)
+
+# Compare the output with "X0"
+if [[ "$output" == "X0" ]]; then
+    echo "Success!"
+else
+    echo "Failed."
+fi
